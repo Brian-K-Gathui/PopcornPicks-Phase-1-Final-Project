95ef38a647d2f73eec6d9d071357d2d6d151c16c
Brian <briankgathui@gmail.com>
2024-10-23 07:32:32 +0300

    Created header with search bar form and search input field

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("PopcornPicks Initialized");
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+    console.log('Search for:', searchTerm);
+});
