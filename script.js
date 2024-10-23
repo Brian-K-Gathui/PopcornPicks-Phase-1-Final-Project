3b5b2d96bb92493b4772cc64a5be4f3046da07a9
Brian <briankgathui@gmail.com>
2024-10-23 07:41:08 +0300

    Integrated TMDB API with async fetch for popular movies

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const API_KEY = 'api_key=your_tmdb_api_key';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+async function getMovies(url) {
+    showLoading();
+    const res = await fetch(url);
+    const data = await res.json();
+    hideLoading();
+    showMovies(data.results);
+}
+
+function showMovies(data) {
+    const main = document.getElementById('main');
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const { title, poster_path } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+            <img src="${IMG_URL + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+            </div>
+        `;
+        main.appendChild(movieEl);
+    });
+}
+
+getMovies(API_URL);
+
+
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
